cbec962221fda6e2d321750d07cad6f4e8541ac7
Tomás Niederhauser <78279996+niederhausertomas@users.noreply.github.com>
2024-12-15 22:28:56 +0100

    fix CSS error

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: [
     './index.html',
     './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html',
   ],
   theme: {
     extend: {
@@ -13,8 +14,15 @@ export default {
         },
         blink: {
           '50%': { 'border-color': 'transparent' },
-        }
-      }
+        },
+        animation: {
+          'marquee': 'marquee 30s linear infinite',
+        },
+        marquee: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
     },
     screens: {
       
@@ -30,27 +38,4 @@ export default {
       })
     },
   ],
-}
-
-
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-    './public/index.html',
-  ],
-  theme: {
-    extend: {
-      animation: {
-        'marquee': 'marquee 30s linear infinite',
-      },
-      keyframes: {
-        marquee: {
-          '0%': { transform: 'translateX(100%)' },
-          '100%': { transform: 'translateX(-100%)' },
-        },
-      },
-    },
-  },
-  plugins: [],
 }
